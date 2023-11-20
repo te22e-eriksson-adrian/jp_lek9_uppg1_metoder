7cee836ddbd03aa730ee954d901fe36f6b66946a
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-11-20 09:33:11 +0100

    gjorde klart uppg1 del B

--- src/App.java
+++ src/App.java
@@ -1,10 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //A
         double omkretsen = omkretsCirkel(9);
         System.out.println("Omkretsen är: "+omkretsen);
+
+        //B
+        ritaLinje(10);
     }
     static double omkretsCirkel(double radie){
             double omkrets = 2*radie*3.14;
             return omkrets;
         }
+    
+    static void ritaLinje(int längd){
+        for (int i=0; i<längd; i++){
+            System.out.print("-");
+        }
+    }
 }
