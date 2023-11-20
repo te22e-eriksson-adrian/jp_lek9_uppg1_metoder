6f056b224f32d9e9734b62e989e6829e13cd175d
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-11-20 09:24:37 +0100

    gjorde klart uppg1 del 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double omkretsen = omkretsCirkel(9);
+        System.out.println("Omkretsen är: "+omkretsen);
     }
+    static double omkretsCirkel(double radie){
+            double omkrets = 2*radie*3.14;
+            return omkrets;
+        }
 }
