1a1f3dab02c2b3a9c818f4927173b42f1ebb4d76
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-11-27 08:36:08 +0100

    gjorde klart uppg1 del c med metod

--- src/App.java
+++ src/App.java
@@ -3,9 +3,14 @@ public class App {
         //A
         double omkretsen = omkretsCirkel(9);
         System.out.println("Omkretsen är: "+omkretsen);
+        System.out.println("      ");
 
         //B
         ritaLinje(10);
+        System.out.println();
+
+        //C
+        ritaRektangel(4, 5);
     }
     static double omkretsCirkel(double radie){
             double omkrets = 2*radie*3.14;
@@ -15,6 +20,14 @@ public class App {
     static void ritaLinje(int längd){
         for (int i=0; i<längd; i++){
             System.out.print("-");
+        }
+    }
+    static void ritaRektangel(int höjd, int bredd){
+        for (int i=0; i<höjd; i++){
+            for (int i2=0; i2<bredd; i2++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
